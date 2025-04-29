347af3f4f891a14a91b665fc4ef85c2e57669b33
Taing Bunsou <bunsou@Taings-MacBook-Pro.local>
2025-04-30 06:36:12 +0700

    Finished Dashboard Layout

--- app/routes/admin/dashboard.tsx
+++ app/routes/admin/dashboard.tsx
@@ -1,6 +1,17 @@
+import {Header} from "../../../components";
+
 const Dashboard = () => {
+    const user = { name: 'Adrian' }
+
     return (
-        <div>Dashboard Page</div>
+        <main className='dashboard wrapper'>
+            <Header
+                title={`Welcome ${user?.name ?? 'Guess'} 👋`}
+                description="Tract activity, trends and popular destinations in real time"
+            />
+
+            Dashboard Page Contents
+        </main>
     )
 }
 export default Dashboard
